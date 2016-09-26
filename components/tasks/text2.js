411ed0684494940f95399d4aecec507e876dfb4d
fran150 <franllopez@yahoo.com.ar>
2016-09-26 01:09:27 -0300

    Test waitReady binding

--- components/tasks/text2.js
+++ components/tasks/text2.js
@@ -2,6 +2,7 @@ define(['quark', 'knockout', 'text!./text2.html'], function($$, ko, template) {
     function Text2Component(params, $scope, $imports) {
         var self = this;
 
+        this.test = ko.observable('Wait Ready Test');
     }
 
     return $$.component(Text2Component, template);
