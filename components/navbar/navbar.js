91d51201ecad811392b4add700d392e8bb5a169f
admin <fllopez@bpba.com.ar>
2016-09-27 18:41:45 -0300

    Errors test

--- components/navbar/navbar.js
+++ components/navbar/navbar.js
@@ -1,5 +1,5 @@
 define(['quark', 'knockout', 'text!./navbar.html'], function($$, ko, template) {
-    function Navbar(params, $scope, $imports) {
+    function Navbar(params, $scope, $imports) {        
     }
 
     return $$.component(Navbar, template);
