b5865c53ef6f7058fcbd000d7ce6bfa65bf31582
fran150 <franllopez@yahoo.com.ar>
2016-09-28 00:15:19 -0300

    Error Handling Test

--- components/navbar/navbar.js
+++ components/navbar/navbar.js
@@ -1,5 +1,6 @@
 define(['quark', 'knockout', 'text!./navbar.html'], function($$, ko, template) {
-    function Navbar(params, $scope, $imports) {        
+    function Navbar(params, $scope, $imports) {
+        this.pepe.papa();
     }
 
     return $$.component(Navbar, template);
